9ea51aca75a84d91c3b7105a6f522cb2fe7f7267
Talha Rana <mtalharana@gmail.com>
2022-08-09 19:03:41 +0500

    challenge 2 done

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -20,7 +20,25 @@ class _PriceScreenState extends State<PriceScreen> {
     return dropdownItems;
   }
 
+  String value = 'no value';
   String selectedCurrency = 'PKR';
+  String selectedCrypto = 'BTC';
+
+  void getvalue() async {
+    String value = await CoinData().getCoindata(selectedCurrency);
+    print(value);
+    setState(() {
+      value = value;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    //14. Call getData() when the screen loads up. We can't call CoinData().getCoinData() directly here because we can't make initState() async.
+    getvalue();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,26 +49,23 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
+          Column(
+            children: [
+              Tickercard(
+                  value: value,
+                  selectedCurrency: selectedCurrency,
+                  crypto: selectedCrypto),
+              Tickercard(
+                value: value,
+                selectedCurrency: selectedCurrency,
+                crypto: selectedCrypto,
               ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
+              Tickercard(
+                value: value,
+                selectedCurrency: selectedCurrency,
+                crypto: selectedCrypto,
               ),
-            ),
+            ],
           ),
           Container(
             height: 150.0,
@@ -64,12 +79,51 @@ class _PriceScreenState extends State<PriceScreen> {
               onChanged: (value) {
                 setState(() {
                   selectedCurrency = value!;
+                  getvalue();
                   print(selectedCurrency);
                 });
               },
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class Tickercard extends StatelessWidget {
+  const Tickercard({
+    Key? key,
+    required this.value,
+    required this.selectedCurrency,
+    required this.crypto,
+  }) : super(key: key);
+
+  final String value;
+  final String selectedCurrency;
+  final String crypto;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 BTC = $value $selectedCurrency ',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
     );
   }
