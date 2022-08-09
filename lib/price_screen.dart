113f110cca153950d5fa71d9a2be438af522e6fa
Talha Rana <mtalharana@gmail.com>
2022-08-09 15:06:20 +0500

    challenge 1 done

---
+++ lib/price_screen.dart
@@ -0,0 +1,76 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, library_private_types_in_public_api, avoid_print
+
+import 'package:flutter/material.dart';
+import 'package:bitcoin_ticker_flutter/coin_data.dart';
+
+class PriceScreen extends StatefulWidget {
+  @override
+  _PriceScreenState createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  List<DropdownMenuItem<String>> getdropdownItemslist() {
+    List<DropdownMenuItem<String>> dropdownItems = [];
+    for (String currency in currenciesList) {
+      dropdownItems.add(DropdownMenuItem(
+        value: currency,
+        child: Text(currency),
+      ));
+    }
+    return dropdownItems;
+  }
+
+  String selectedCurrency = 'PKR';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('🤑 Coin Ticker'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 BTC = ? USD',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 150.0,
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(bottom: 30.0),
+            color: Colors.lightBlue,
+            child: DropdownButton<String>(
+              value: selectedCurrency,
+              // ignore: prefer_const_literals_to_create_immutables
+              items: getdropdownItemslist(),
+              onChanged: (value) {
+                setState(() {
+                  selectedCurrency = value!;
+                  print(selectedCurrency);
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
